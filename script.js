b2d930aff98db8d34fa6a566d720ba4e922e2699
Samuel Alvarenga <69541465+samueledu10@users.noreply.github.com>
2023-07-18 21:24:07 -0400

    Add function that does basic math operations

---
+++ script.js
@@ -0,0 +1,19 @@
+function operate(leftOp, operator, rightOp) {
+    switch(operator) {
+        case "+":
+            return leftOp + rightOp;
+        case "-":
+            return leftOp - rightOp;
+        case "*":
+            return leftOp * rightOp;
+        case "/":
+            if(rightOp === 0) {
+                return "ERROR";
+            }
+            return leftOp / rightOp;
+        default:
+            return "ERROR";
+    }
+}
+
+console.log(operate(10, "%", 10));
